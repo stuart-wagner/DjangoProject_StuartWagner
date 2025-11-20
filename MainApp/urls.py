d9d6ac09b9cd5f8a6e7468051675803a14fac2cc
stuart-wagner <ww289546@icloud.com>
2025-11-20 15:15:22 -0600

    end of class 11/20

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('topics/', views.topics, name='topics'),
     path('topics/<int:topic_id>/', views.topic, name='topic'),
+    path('new_topic', views.new_topic, name='new_topic'),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import *
 from .models import *
 
 # Create your views here.
@@ -11,4 +12,24 @@ def topics(request):
     return render(request, 'MainApp/topics.html', context)
 
 def topic(request, topic_id):
-    
+    t = Topic.objects.get(id=topic_id)
+    entries = Entry.objects.filter(topic=t).order_by('-date_added') #the minus (-) sign means descending order
+    context = {'topic':t, 'entries':entries}
+
+    return render(request, 'MainApp/topic.html', context)
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('MainApp:topics')
+        
+    context = {'form':form}
+    return render(request, 'MainApp/new_topic.html', context)
+
+
