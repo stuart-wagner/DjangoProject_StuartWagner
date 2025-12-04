20eb7dc82921ec71b9471fb284ea982cd15a9394
stuart-wagner <ww289546@icloud.com>
2025-12-04 14:33:59 -0600

    12/4 class update 1

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [
+    path('',include('django.contrib.auth.urls')),
+    path('logout/',auth_views.LogoutView.as_view(),name='logout'),
+]
